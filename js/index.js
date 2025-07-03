56ba1069838c356d7e84897b26160d51f6fd3685
moongsoon <jmlee710000@gmail.com>
2025-07-03 11:57:40 +0900

    로고 반응형 + 애니메이션 구현

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
+function updateLogoMode() {
+  const wrapper = document.getElementById('mainLogoWrapper');
+  const w = window.innerWidth;
+  const h = window.innerHeight;
 
-  // 애니메이션 끝난 뒤 헤더 텍스트 생성
-  setTimeout(() => {
-    const header = document.getElementById("header");
-    header.innerHTML = `PLANEARTH ARCHITECTS`;
-    header.style.opacity = 1;
-  }, 3400); // .main-logo 애니메이션 끝나는 시점과 맞춤!
+  if (w > h) {
+    wrapper.classList.add('one-line');
+    wrapper.classList.remove('two-line');
+  } else {
+    wrapper.classList.add('two-line');
+    wrapper.classList.remove('one-line');
+  }
+}
+
+window.addEventListener('load', updateLogoMode);
+window.addEventListener('resize', updateLogoMode);
